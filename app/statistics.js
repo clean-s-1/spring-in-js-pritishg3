d49c9d9ec235b8c52de2012239dd3c783c3559fd
pritishg3 <69862319+pritishg3@users.noreply.github.com>
2022-05-06 16:09:45 +0530

    Update statistics.js
    Initial commit

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,20 @@
 module.exports=function getStatistics(numbers){
     
     //implement the computation of statistics here
+    var min = 0;      
+    var max = 0;
+    var average = 0;  
+
+   
+    if(numbers.size === 0){
+        min = NaN;
+        max = NaN;
+        average = NaN;
+    } 
+    else {
+        min = Math.min.apply(null, numbers);
+        max = Math.max.apply(null, numbers);
+        average = numbers.reduce((a, b) => { return a + b; }) / numbers.length;
+    }
 }
 
